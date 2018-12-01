507015522d3b854778baf399feb4f8de66c8e5d7
Raiyan <raiyan.quaium@carleton.ca>
2018-11-30 22:40:42 -0500

    Can generate a word cloud

--- motley foot scraper.py
+++ motley foot scraper.py
@@ -4,23 +4,36 @@ from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup as soup
 import pandas as pd
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 for i in range(5):
-
-#Open a connection to retreive the page and close connection after
 	url = 'https://www.fool.ca/recent-headlines/' + '/page/' + str(i) + '/'
-	print(url)
+	
 	req = Request(url , headers={'User-Agent': 'Mozilla/5.0'})
 	webpage = urlopen(req).read()
-	
 	page_soup = soup(webpage, "html.parser")
 	containers = page_soup.findAll("p","promo")
 
 	for container in containers:
 		print(container)
+		f= open("headlines.txt","a+")
+		f.write(str(container.getText())) 
 
+text = open('headlines.txt').read()
 
-#Scrape the HTML of the Harry Rosen New Products Page for information on brand, product name and price
-#page_soup = soup(page_html, "html.parser")
-#containers = soup.find_all("p", "promo")
+# Generate a word cloud image
+wordcloud = WordCloud().generate(text)
 
+# Display the generated image:
+# the matplotlib way:
+
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+
+# lower max_font_size
+wordcloud = WordCloud(max_font_size=40).generate(text)
+plt.figure()
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
+		
